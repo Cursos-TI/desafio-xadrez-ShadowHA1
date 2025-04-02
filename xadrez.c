3f9c2710431caffe69dc7ae6af2e79eda5015dcd
ShadowHA1 <bruno.ferreh20@gmail.com>
2025-04-02 23:56:43 +0000

    Criei movimentos do Xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int i;
+    
+    printf("Desafio de Xadrez!!\n");
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
+    // Movimento da Torre 'for'
+    printf("Movimento da Torre:\n");
+    for (i = 0; i < 5; i++) { // incremento = 0, incrimento menor que 5, incremento mudará
+        printf("Direita\n"); // imprime a movimentação da peça
+    }
+    
+    printf("\n");
+    
+    // Movimento do Bispo usando 'while'
+    printf("Movimento do Bispo:\n");
+    i = 0;
+    while (i < 5) {
+        printf("Cima, Direita\n"); // imprime a movimentação da peça
+        i++;
+    }
+    
+    printf("\n");
+    
+    // Movimento da Rainha 'do-while'
+    printf("Movimento da Rainha:\n");
+    i = 0;
+    do {
+        printf("Esquerda\n"); // imprime a movimentação da peça
+        i++;
+    } while (i < 8);
+    
     return 0;
 }
